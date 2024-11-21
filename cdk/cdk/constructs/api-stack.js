835c9a661b06dd062ddc8fa835fd086b85ab841a
Jorge Pablos-Araque <jpablos11@gmail.com>
2024-11-21 06:08:39 +0000

    p

---
+++ cdk/cdk/constructs/api-stack.js
@@ -0,0 +1,26 @@
+const { Stack } = require('aws-cdk-lib')
+const { Runtime, Code, Function } = require('aws-cdk-lib/aws-lambda')
+const { RestApi, LambdaIntegration } = require('aws-cdk-lib/aws-apigateway')
+
+class ApiStack extends Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props)
+
+    const lambdaFunction = new Function(this, 'HandlerFunction', {
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'handler.hello',
+      code: Code.fromAsset('functions'),
+    })
+
+    const api = new RestApi(this, `${props.stageName}-MyApi`, {
+      deployOptions: {
+        stageName: props.stageName
+      }
+    })
+
+    const lambdaIntegration = new LambdaIntegration(lambdaFunction)
+    api.root.addMethod('GET', lambdaIntegration)
+  }
+}
+
+module.exports = { ApiStack }
